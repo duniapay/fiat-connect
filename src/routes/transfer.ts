039caada02b5fd35a3c4b9d22b0ae7695e619fdd
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2022-09-13 12:01:54 +0000

    Update transfer.ts

--- src/routes/transfer.ts
+++ src/routes/transfer.ts
@@ -167,9 +167,7 @@ export function transferRouter({
               transferAddress: entity.transferAddress,
             })
         } catch (error: any) {
-        
-          
-         
+              console.log(error)         
               res
               .status(409)
               .send({ error: FiatConnectError.ResourceExists })
