4f78e7118a0a116f57018d3ddc2a7772c901d360
serge <sergewilfried@outlook.fr>
2022-09-14 15:41:52 +0000

    fix: update script to match folder name

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-node dist/index.js --auth-config-option $AUTH_CONFIG_OPTION --session-secret $SESSION_SECRET
+node build/src/index.js --auth-config-option $AUTH_CONFIG_OPTION --session-secret $SESSION_SECRET
