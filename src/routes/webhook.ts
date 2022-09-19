767c3b090d2e0d6639296142979127bde5333101
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2022-09-19 16:39:00 +0000

    Create webhook.ts
    feat: add support for webhook events

---
+++ src/routes/webhook.ts
@@ -0,0 +1,52 @@
+
+import axios from 'axios'
+import * as dotenv from 'dotenv'
+import express from 'express'
+import { asyncRoute } from './async-route'
+
+const crypto = require('crypto');
+
+dotenv.config()
+
+
+
+
+
+export function webhookRouter({
+    clientAuthMiddleware
+  }: {
+    clientAuthMiddleware: express.RequestHandler[]
+    client: any
+    dataSource: any
+  }): express.Router {
+    const router = express.Router()
+    router.use(clientAuthMiddleware)
+    router.post(
+        '/webhook',
+        asyncRoute(
+          async (
+            _req: express.Request,
+            _res: express.Response,
+          ) => {
+            const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+            const PROVIDER_ID = process.env.PROVIDER_ID
+            const WEBHOOK_URL = `${process.env.WEBHOOK_URL}/${PROVIDER_ID}`
+
+            const webhook_payload = _req.body
+
+            const hmac = crypto.createHmac('sha1', WEBHOOK_SECRET)
+
+            const webhookDigest = hmac.update(webhook_payload).digest('hex')
+            const t = Date.now();
+            const options= {
+                headers: {
+                    "FiatConnect-Signature" : `t=${t},s=${webhookDigest}`
+                },
+                body: {...webhook_payload}
+            }
+                return await axios.post(WEBHOOK_URL, options)
+          }));
+
+    
+    return router
+}
