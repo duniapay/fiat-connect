13f6fa1bca05dc373418975ee09fc317a279e339
SergeWilfried <63460822+SergeWilfried@users.noreply.github.com>
2022-10-11 21:56:40 +0000

    Update webhook.ts
    Refactor implementation

--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -5,43 +5,47 @@ import { createHmac } from 'crypto'
 
 import { asyncRoute } from './async-route'
 import { WebhookEvent } from '../types'
+import {
+  WebhookEventType,
+  WebhookRequestBody,
+} from '@fiatconnect/fiatconnect-types'
 
 dotenv.config()
 
-export function webhookRouter({
-  clientAuthMiddleware,
-}: {
-  clientAuthMiddleware: express.RequestHandler[]
-}): express.Router {
-  const router = express.Router()
-  router.use(clientAuthMiddleware)
-  router.post(
-    '/',
-    asyncRoute(async (_req: express.Request, _res: express.Response) => {
-      const WEBHOOK_URL = _req.body?.url
-      const options = {
-        baseUrl: 'http://api-staging.dunia.africa',
-        body: JSON.stringify(WEBHOOK_URL),
-      }
-      // Save webhook url
-      return axios.post('webhook', options)
-    }),
-  )
-  return router
+export async function notifyPartner(
+  e: WebhookRequestBody<WebhookEventType>,
+  secret: any,
+) {
+  processSomething(() => {
+    const providerId = 'dunia-payment'
+    const baseUrl =
+      'https://liquidity-dot-celo-mobile-alfajores.appspot.com/fiatconnect/webhook/' +
+      providerId
+
+    /**
+     * Your API call to webhookUrl with
+     * your defined body about status of event
+     */
+    const hmac = createHmac('sha1', secret)
+
+    const webhookDigest = hmac.update(JSON.stringify(e)).digest('hex')
+    const t = `t=` + Date.now()
+    const s = `v1=` + webhookDigest
+
+    axios.post(
+      baseUrl,
+      { body: JSON.stringify(e) },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'fiatconnect-signature': t + ',' + s,
+        },
+      },
+    )
+  })
+  return true
 }
 
-export async function notifyPartner(e: WebhookEvent, secret: any) {
-  const hmac = createHmac('sha1', secret)
-
-  const webhookDigest = hmac.update(e.data).digest('hex')
-  const t = Date.now()
-  const unformattedBody = e.data
-
-  const options = {
-    headers: {
-      'FiatConnect-Signature': `t=${t},s=${webhookDigest}`,
-    },
-    body: JSON.stringify({ ...unformattedBody }),
-  }
-  return axios.post(e.url, options)
+const processSomething = (callback: any) => {
+  setTimeout(callback, 10000)
 }
